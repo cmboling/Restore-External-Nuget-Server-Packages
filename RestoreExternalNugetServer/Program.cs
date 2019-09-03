f01a8346c2358cb29406a68b38e6b2330d475441
Lautaro Carro <luisb@lagash.com>
2019-09-03 13:49:29 -0300

    Initial Commit Of Project

---
+++ RestoreExternalNugetServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GoldAdmin.Entity;
+
+namespace RestoreExternalNugetServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            Console.WriteLine("The class of de Users is: " + typeof(User).FullName);
+        }
+    }
+}
